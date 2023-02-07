d81751899da9d4587da71bb3cd4edda9c43f8b2c
MarceloMosl <marcelomosl@icloud.com>
2023-02-06 21:30:04 -0300

    Initial Configs

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(json());
+
+app.listen(PORT, () => console.log("Server On"));
