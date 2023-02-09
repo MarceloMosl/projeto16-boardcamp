dd3c31e55d24c03aedc4ee4ace041783472b405d
MarceloMosl <marcelomosl@icloud.com>
2023-02-09 14:31:58 -0300

    Connection w/ Db

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import express, { json } from "express";
 import cors from "cors";
+import { db } from "./config/database.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(json());
+
+app.get("/teste", async (req, res) => {
+  const tst = await db.query("SELECT * FROM customers");
+
+  res.send(tst.rows);
+});
 
 app.listen(PORT, () => console.log("Server On"));
