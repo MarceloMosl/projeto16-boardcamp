5929c77e47d12ef1167a3d1cad1fdf826206c01b
MarceloMosl <marcelomosl@icloud.com>
2023-02-12 21:43:55 -0300

    Feat: Post Rentals

--- src/controllers/rentsCont.js
+++ src/controllers/rentsCont.js
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { db } from "../config/database.js";
 
 export async function getRentals(req, res) {
@@ -25,4 +26,49 @@ export async function getRentals(req, res) {
   await rents.rows.map((a) => rentsPromise.push(a.json_build_object));
 
   res.send(rentsPromise);
+}
+
+export async function postRentals(req, res) {
+  const { customerId, gameId, daysRented } = req.body;
+
+  const customerExist = await db.query(
+    `SELECT * FROM customers WHERE id = $1`,
+    [customerId]
+  );
+
+  const gameExist = await db.query(`SELECT * FROM games WHERE id = $1`, [
+    gameId,
+  ]);
+
+  if (gameExist.rows.length === 0 || customerExist.rows.length === 0)
+    return res.sendStatus(400);
+
+  const gameInStock = await db.query(
+    `SELECT * FROM rentals where "gameId" = $1`,
+    [gameId]
+  );
+
+  if (gameInStock.length - gameExist.rows[0].stockTotal < 0)
+    return res.sendStatus(400);
+
+  try {
+    await db.query(
+      `insert into rentals
+        ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+        values ($1,$2,$3,$4,$5,$6, $7)`,
+      [
+        customerId,
+        gameId,
+        dayjs().format("YYYY/MM/DD"),
+        daysRented,
+        null,
+        daysRented * gameExist.rows[0].pricePerDay,
+        null,
+      ]
+    );
+
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.send(error);
+  }
 }
--- src/routers/rentalsRoute.js
+++ src/routers/rentalsRoute.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getRentals } from "../controllers/rentsCont.js";
+import { getRentals, postRentals } from "../controllers/rentsCont.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import schemaRental from "../schemas/rentalsSchema.js";
 
 const rentalsRoute = Router();
 
 rentalsRoute.get("/rentals", getRentals);
+rentalsRoute.post("/rentals", validateSchema(schemaRental), postRentals);
 
 export default rentalsRoute;
